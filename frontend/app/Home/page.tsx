9f5f9d66df788942843bef0f8f134e5ad27e7699
Lawrence Luo <luolawrence1@gmail.com>
2026-01-01 11:44:27 -0800

    #1 JWT token persists in frontend after login and is cleared on sign out

--- frontend/app/Home/page.tsx
+++ frontend/app/Home/page.tsx
@@ -10,6 +10,11 @@ type User = {
   updatedAt: string
 }
 
+
+const signOut = () => {
+  localStorage.removeItem('token');
+}
+
 const Home = (): JSX.Element => {
   const { data, loading, error } = useGetUsers();
 
@@ -29,7 +34,10 @@ const Home = (): JSX.Element => {
   return (
     <main>
       <header>
-        <h1>Home</h1>
+        <div className="display-flex width-full">
+          <h1>Home</h1>
+          <button onClick={signOut} className="p-5 cursor-pointer border-1 border-white border-solid radius-sm">Sign Out</button>
+        </div>
       </header>
 
       <section>
